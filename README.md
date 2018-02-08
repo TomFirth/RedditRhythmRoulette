e849d9f26bcad49928cf67709bbeba69564cc1c8
Tom Firth <tom.firth@holidayextras.com>
2018-02-08 13:26:39 +0000

    random tracks

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reddit Rhythm Roulette
+
+Scrape Reddit (subreddit: vintageobscura) for three random tracks.
+
+# Nice to Haves:
+
+- Store the records for a period of time
+- form to let users submit their tracks
---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const port = 3000
+const { fetchSubreddit } = require('fetch-subreddit')
+require('dotenv').config()
+let randomTracks = []
+
+function randomify(max) {
+  return Math.floor(Math.random() * (max - 0 + 1)) + 0;
+}
+
+const requestHandler = (req, res) => {
+  fetchSubreddit('vintageobscura')
+  .then((urls) => {
+    for (let i = 0; i < 3; i++) {
+      const rand = randomify(urls[0].urls.length)
+      randomTracks.push({
+        track: urls[0].urls[rand]
+      })
+    }
+    const tracks = randomTracks.slice(0, 3)
+    res.end(JSON.stringify(tracks))
+  })
+  .catch((err) => console.error(err))
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) console.error(err)
+  console.log(`server is listening on ${port}`)
+})
